0ce157b2db4f3d4c14f64352f3ff99009fbeeda6
joseph <gilgenlabs@gmail.com>
2024-06-21 09:12:33 -0700

    feat: better handle inbound emails

--- server/src/smtp.ts
+++ server/src/smtp.ts
@@ -9,20 +9,20 @@ import bcrypt from "bcrypt"
 export const smtpServer = new SMTPServer({
   logger: false,
   authOptional: true,
-  // disabledCommands: ["AUTH"],
   disableReverseLookup: true,
   maxClients: 5,
   onAuth(auth, _session, callback) {
     console.info("SMTP Auth:", auth)
-
+    if (!auth.username || !auth.password) {
+      return callback(new Error("Invalid authentication"))
+    }
     const username = `${auth.username}@${process.env.MAIL_DOMAIN}`.toLowerCase()
     Mailboxes.findOne({ username }).then((mailbox) => {
-      console.log(mailbox)
       if (!mailbox) {
         return callback(new Error("Invalid authentication"))
       }
 
-      const isPasswordValid = bcrypt
+      bcrypt
         .compare(auth.password, mailbox.password)
         .then((isPasswordValid) => {
           if (!isPasswordValid) {
@@ -69,24 +69,84 @@ export const smtpServer = new SMTPServer({
         return callback(new Error("Error inserting email"))
       }
 
-      try {
-        Mailboxes.updateOne(
-          { username: user },
-          {
-            $push: {
-              emails: {
-                emailId: emailId,
-                sender: mail.from?.value[0].address ?? null,
-                subject: mail.subject ?? null,
-                date: mail.date ?? new Date(),
-                isRead: false,
+      if (user) {
+        try {
+          Mailboxes.updateOne(
+            { username: user },
+            {
+              $push: {
+                emails: {
+                  emailId: emailId,
+                  sender: mail.from?.value[0].address ?? null,
+                  subject: mail.subject ?? null,
+                  date: mail.date ?? new Date(),
+                  isRead: false,
+                },
               },
             },
-          },
-          { upsert: true }
-        )
-      } catch (e) {
-        console.error(e)
+            { upsert: true }
+          )
+        } catch (e) {
+          console.error(e)
+        }
+      }
+      const to = mail.to
+      if (Array.isArray(to)) {
+        for (const address of to) {
+          const emails = address.value
+          for (const email of emails) {
+            const username = email.address
+            // already pushed to user's mailbox
+            if (username === user) {
+              continue
+            }
+            try {
+              Mailboxes.updateOne(
+                { username },
+                {
+                  $push: {
+                    emails: {
+                      emailId: emailId,
+                      sender: mail.from?.value[0].address ?? null,
+                      subject: mail.subject ?? null,
+                      date: mail.date ?? new Date(),
+                      isRead: false,
+                    },
+                  },
+                }
+              )
+            } catch (e) {
+              console.error(e)
+            }
+          }
+        }
+      } else {
+        const emails = to.value
+        for (const email of emails) {
+          const username = email.address
+          // already pushed to user's mailbox
+          if (username === user) {
+            continue
+          }
+          try {
+            Mailboxes.updateOne(
+              { username },
+              {
+                $push: {
+                  emails: {
+                    emailId: emailId,
+                    sender: mail.from?.value[0].address ?? null,
+                    subject: mail.subject ?? null,
+                    date: mail.date ?? new Date(),
+                    isRead: false,
+                  },
+                },
+              }
+            )
+          } catch (e) {
+            console.error(e)
+          }
+        }
       }
     })
   },
