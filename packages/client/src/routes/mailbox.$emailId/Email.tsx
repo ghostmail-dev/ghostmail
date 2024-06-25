266dec742507a5fe82fb136806fc58924a8b01d6
joseph <gilgenlabs@gmail.com>
2024-06-24 17:28:19 -0700

    fix types

--- packages/client/src/routes/mailbox.$emailId/Email.tsx
+++ packages/client/src/routes/mailbox.$emailId/Email.tsx
@@ -1,15 +1,15 @@
-import { useNavigate, useParams } from 'react-router-dom'
-import { EmailHeader } from './components/email-header'
-import { useGetEmailQuery } from './gql/get-email.operation'
-import { ArrowLeftIcon } from '@heroicons/react/20/solid'
-import { Attachments } from './components/attachments'
+import { useNavigate, useParams } from "react-router-dom"
+import { EmailHeader } from "./components/email-header"
+import { useGetEmailQuery } from "./gql/get-email.operation"
+import { ArrowLeftIcon } from "@heroicons/react/20/solid"
+import { Attachments } from "./components/attachments"
 
 export const Email = () => {
   const { emailId } = useParams()
   const navigate = useNavigate()
   const { data, loading, error } = useGetEmailQuery({
     variables: {
-      id: emailId ?? '',
+      id: emailId ?? "",
     },
   })
   if (loading || data === undefined) return <div>Loading...</div>
@@ -20,17 +20,17 @@ export const Email = () => {
     email && (
       <div>
         <button className={`btn mt-4 ml-4`} onClick={() => navigate(-1)}>
-          <ArrowLeftIcon className='w-4 h-4 mr-2' />
+          <ArrowLeftIcon className="w-4 h-4 mr-2" />
           Back
         </button>
         <EmailHeader email={email} />
-        <div className='shadow-md rounded-lg px-4 m-4 border border-gray-200'>
+        <div className="shadow-md rounded-lg px-4 m-4 border border-gray-200">
           <div
             dangerouslySetInnerHTML={{
-              __html: email.html ?? email.textAsHtml ?? email.text ?? '',
+              __html: email.html ?? email.textAsHtml ?? email.text ?? "",
             }}
           />
-          <Attachments attachments={email.attachments} />
+          {email.attachments && <Attachments attachments={email.attachments} />}
         </div>
       </div>
     )
--- packages/client/src/routes/mailbox.$emailId/components/attachments.tsx
+++ packages/client/src/routes/mailbox.$emailId/components/attachments.tsx
@@ -1,27 +1,27 @@
-import { ArrowDownIcon, DocumentIcon } from '@heroicons/react/20/solid';
-import { GetEmailQuery } from '../gql/get-email.operation';
+import { ArrowDownIcon, DocumentIcon } from "@heroicons/react/20/solid"
+import { GetEmailQuery } from "../gql/get-email.operation"
 
 export const Attachments = ({
   attachments,
 }: {
-  attachments: NonNullable<GetEmailQuery['email']>['attachments'][];
+  attachments: NonNullable<NonNullable<GetEmailQuery["email"]>["attachments"]>
 }) => {
-  const attachmentCount = attachments.length;
+  const attachmentCount = attachments.length
   return (
     <div>
-      <h2 className='prose-sm mb-0'>
+      <h2 className="prose-sm mb-0">
         {attachmentCount >= 1
-          ? `${attachmentCount} attachment${attachmentCount > 1 ? 's' : ''}`
-          : ''}
+          ? `${attachmentCount} attachment${attachmentCount > 1 ? "s" : ""}`
+          : ""}
       </h2>
 
-      <div className='flex items-center flex-col justify-start'>
+      <div className="flex items-center flex-col justify-start">
         {attachments?.map((attachment: any, index: number) => (
           <div
             key={index}
-            className='flex items-center justify-start w-full p-2'
+            className="flex items-center justify-start w-full p-2"
           >
-            <DocumentIcon className='w-6 h-6 mr-2' />
+            <DocumentIcon className="w-6 h-6 mr-2" />
             <a
               href={`data:application/octet-stream;base64,${attachment.content}`}
               download={attachment.filename}
@@ -34,17 +34,17 @@ export const Attachments = ({
                 attachment.generatedFileName
               )}
             </a>
-            <span className='text-xs text-gray-400 ml-2'>
-              {attachment.size} kb{attachment.size > 1 ? 's' : ''}
+            <span className="text-xs text-gray-400 ml-2">
+              {attachment.size} kb{attachment.size > 1 ? "s" : ""}
             </span>
-            <div className='tooltip ml-4' data-tip='Download'>
-              <button className='btn btn-sm btn-square'>
-                <ArrowDownIcon className='w-5 h-5' />
+            <div className="tooltip ml-4" data-tip="Download">
+              <button className="btn btn-sm btn-square">
+                <ArrowDownIcon className="w-5 h-5" />
               </button>
             </div>
           </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
