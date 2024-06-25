77d91bb489426244a85ac9f29d1ffc65f01b147a
joseph <gilgenlabs@gmail.com>
2024-06-25 10:29:21 -0700

    add logout

---
+++ packages/front-end/app/routes/logout/route.tsx
@@ -0,0 +1,9 @@
+import { LoaderFunction } from "@remix-run/node"
+import { authenticator } from "~/services/auth.server"
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await authenticator.logout(request, {
+    redirectTo: "/login",
+  })
+  return null
+}
