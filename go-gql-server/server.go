4df98b5d360ffd2687f76b06a092a31beeb3a2ab
joseph <gilgenlabs@gmail.com>
2024-07-01 09:55:51 -0700

    feat: initial setup of a go graphql server

---
+++ go-gql-server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"jb-1980/ghostmail/go-gql-server/graph"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173", "https://studio.apollographql.com"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "example.org"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
+	router.Handle("/graphql", srv)
+
+	err := http.ListenAndServe(":4040", router)
+	if err != nil {
+		panic(err)
+	}
+}
